909dfb2d0517af59ab14068eda041e5861c760c8
aya25 <eng.aya93g@gmail.com>
2023-06-05 12:55:18 -0700

    comp-6-5aa-2023

---
+++ lib/model/lastCommentClientModel.dart
@@ -0,0 +1,34 @@
+import 'package:crm_smart/model/commentmodel.dart';
+
+import 'clientmodel.dart';
+
+class LastcommentClientModel {
+  LastcommentClientModel({
+    required this.client_obj,
+  });
+
+  ClientModel? client_obj;
+
+  List<CommentModel>? allComment;
+  String? dateCommentClient;
+  String? hoursLastComment;
+
+  LastcommentClientModel.fromJson(Map<String, dynamic> jsondata) {
+    client_obj =
+        jsondata['client_obj'] = ClientModel.fromJson(jsondata['client_obj']);
+    allComment = jsondata['allComment'] = jsondata['allComment'].map((e) {
+      CommentModel.fromJson(e);
+    }).toList();
+    CommentModel.fromJson(jsondata['allComment']);
+
+    dateCommentClient = jsondata['dateCommentClient'];
+    hoursLastComment = jsondata['hoursLastComment'];
+    // type = json['type'];
+  }
+
+  Map<String, dynamic> toJson() {
+    final _data = <String, dynamic>{};
+
+    return _data;
+  }
+}
