b3ba5e9ca510a57eb5a44a0554990d676a60d7b0
aya <aya.ghoury@gmail.com>
2023-08-02 09:53:58 +0300

    commmit user and care

--- lib/ui/screen/care/communcation_view_widget.dart
+++ lib/ui/screen/care/communcation_view_widget.dart
@@ -125,156 +125,157 @@ class _communcation_view_widgetState extends State<communcation_view_widget> {
                 ],) : Container(),
             ],
           ),
-        ):
-        Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: Column(
-            children: [
-              widget.element.typeCommuncation=='دوري'?
-              CheckboxListTile(
-                title: new Text('لا يستخدم النظام'),
-                value:   typepayController,// as bool,
-                onChanged: (bool? value) {
-                  setState(() {
-                    print(value);
-                    typepayController=value!;
-                    //values[key] = value;
-                  });
-                },
-              ):Container(),
-              widget.element.typeCommuncation=='دوري'?
-              SizedBox(height: 20,):Container(),
-              widget.element.typeCommuncation=='دوري'?
-              CheckboxListTile(
-                title: new Text('لايوجد رقم هاتف-أو الرقم خاطئ'),
-                value: numberwrong,// as bool,
-                onChanged: (bool? value) {
-                  setState(() {
-                    print(value);
-                    numberwrong=value!;
-                    //values[key] = value;
-                  });
-                },
-              ):Container(),
-
-              widget.element.typeCommuncation=='دوري'?
-              CheckboxListTile(
-                title: new Text('العميل متكرر'),
-                value:   repeat,// as bool,
-                onChanged: (bool? value) {
-                  setState(() {
-                    print(value);
-                    repeat=value!;
-                    //values[key] = value;
-                  });
-                },
-              ):Container(),
-              widget.element.typeCommuncation=='تركيب'|| widget.element.typeCommuncation=='دوري'?
-              Row(
-                children: [
-                  Text('التقييم 1/5'),
-                  RatingBar.builder(
-                    initialRating: 0.0,
-                    minRating: 0.0,
-                    direction: Axis.horizontal,
-                    allowHalfRating: false,
-                    itemCount: 5,
-                    itemPadding: EdgeInsets.symmetric(horizontal: 4.0),
-                    itemBuilder: (context, _) => Icon(
-                      Icons.star,
-                      color: Colors.amber,
-                    ),
-                    onRatingUpdate: (rating) {
-                      print(rating);
-                      setState(() {
-                        rate=rating;
-                        print(rate);
-                      });
-                    },
-                  ),
-                ],):Container(),
-              ElevatedButton(
-                  style: ButtonStyle(
-
-                      backgroundColor: MaterialStateProperty.all(
-                          kMainColor
-                      )),
-                  onPressed: () async {
-                    if(widget.element.typeCommuncation!='دوري') {
-                      Provider.of<communication_vm>
-                        (context,listen: false).addcommmuncation({
-                        //'fk_client':widget.com.fkClient.toString(),
-                        'fk_user':Provider.of<user_vm_provider>
-                          (context,listen: false).currentUser.idUser.toString(),
-                        'date_communication':DateTime.now().toString(),
-                        'result':'0',//
-                        'nameUser':Provider.of<user_vm_provider>
-                          (context,listen: false).currentUser.nameUser.toString(),
-                        'type_install':widget.element.type_install.toString(),
-                        'id_invoice':widget.element.id_invoice.toString(),
-                        'rate':rate.toString(),
-
-                      },widget.element.idCommunication,
-                          widget.element.type_install==null?
-                          1:
-                          int.parse(widget.element.type_install.toString())).then((value) =>
-
-                          clear(value)
-                      );
-                    }else{
-    Provider.of<communication_vm>(context,
-    listen: false).isloadval(true);
-    await Provider.of<config_vm>(context, listen: false).getAllConfig();
-    List<ConfigModel> _listconfg =
-    Provider.of<config_vm>(context, listen: false)
-        .listofconfig;
-
-    peroid =
-    _listconfg.firstWhere((element) =>
-    element.name_config == 'period_commincation3');
-
-    DateTime datanext=DateTime.now();
-
-    int peroidtime= int.parse(peroid.value_config);
-    datanext=Jiffy().add(days: peroidtime).dateTime;
-    print(datanext.toString());
-    // datanext.add(Duration(days: peroidtime));
-    print(datanext);
-    print( widget.element.dateCommunication.toString());
-    print( widget.element.idCommunication);
-    if(widget.element.dateCommunication==null) {
-    //datanext.add( Duration(days: day+ pp));
+        ):Container())
+         //         :
+    //     Padding(
+    //       padding: const EdgeInsets.all(8.0),
+    //       child: Column(
+    //         children: [
+    //           widget.element.typeCommuncation=='دوري'?
+    //           CheckboxListTile(
+    //             title: new Text('لا يستخدم النظام'),
+    //             value:   typepayController,// as bool,
+    //             onChanged: (bool? value) {
+    //               setState(() {
+    //                 print(value);
+    //                 typepayController=value!;
+    //                 //values[key] = value;
+    //               });
+    //             },
+    //           ):Container(),
+    //           widget.element.typeCommuncation=='دوري'?
+    //           SizedBox(height: 20,):Container(),
+    //           widget.element.typeCommuncation=='دوري'?
+    //           CheckboxListTile(
+    //             title: new Text('لايوجد رقم هاتف-أو الرقم خاطئ'),
+    //             value: numberwrong,// as bool,
+    //             onChanged: (bool? value) {
+    //               setState(() {
+    //                 print(value);
+    //                 numberwrong=value!;
+    //                 //values[key] = value;
+    //               });
+    //             },
+    //           ):Container(),
+    //
+    //           widget.element.typeCommuncation=='دوري'?
+    //           CheckboxListTile(
+    //             title: new Text('العميل متكرر'),
+    //             value:   repeat,// as bool,
+    //             onChanged: (bool? value) {
+    //               setState(() {
+    //                 print(value);
+    //                 repeat=value!;
+    //                 //values[key] = value;
+    //               });
+    //             },
+    //           ):Container(),
+    //           widget.element.typeCommuncation=='تركيب'|| widget.element.typeCommuncation=='دوري'?
+    //           Row(
+    //             children: [
+    //               Text('التقييم 1/5'),
+    //               RatingBar.builder(
+    //                 initialRating: 0.0,
+    //                 minRating: 0.0,
+    //                 direction: Axis.horizontal,
+    //                 allowHalfRating: false,
+    //                 itemCount: 5,
+    //                 itemPadding: EdgeInsets.symmetric(horizontal: 4.0),
+    //                 itemBuilder: (context, _) => Icon(
+    //                   Icons.star,
+    //                   color: Colors.amber,
+    //                 ),
+    //                 onRatingUpdate: (rating) {
+    //                   print(rating);
+    //                   setState(() {
+    //                     rate=rating;
+    //                     print(rate);
+    //                   });
+    //                 },
+    //               ),
+    //             ],):Container(),
+    //           ElevatedButton(
+    //               style: ButtonStyle(
+    //
+    //                   backgroundColor: MaterialStateProperty.all(
+    //                       kMainColor
+    //                   )),
+    //               onPressed: () async {
+    //                 if(widget.element.typeCommuncation!='دوري') {
+    //                   Provider.of<communication_vm>
+    //                     (context,listen: false).addcommmuncation({
+    //                     //'fk_client':widget.com.fkClient.toString(),
+    //                     'fk_user':Provider.of<user_vm_provider>
+    //                       (context,listen: false).currentUser.idUser.toString(),
+    //                     'date_communication':DateTime.now().toString(),
+    //                     'result':'0',//
+    //                     'nameUser':Provider.of<user_vm_provider>
+    //                       (context,listen: false).currentUser.nameUser.toString(),
+    //                     'type_install':widget.element.type_install.toString(),
+    //                     'id_invoice':widget.element.id_invoice.toString(),
+    //                     'rate':rate.toString(),
+    //
+    //                   },widget.element.idCommunication,
+    //                       widget.element.type_install==null?
+    //                       1:
+    //                       int.parse(widget.element.type_install.toString())).then((value) =>
+    //
+    //                       clear(value)
+    //                   );
+    //                 }else{
+    // Provider.of<communication_vm>(context,
+    // listen: false).isloadval(true);
+    // await Provider.of<config_vm>(context, listen: false).getAllConfig();
+    // List<ConfigModel> _listconfg =
+    // Provider.of<config_vm>(context, listen: false)
+    //     .listofconfig;
+    //
+    // peroid =
+    // _listconfg.firstWhere((element) =>
+    // element.name_config == 'period_commincation3');
+    //
+    // DateTime datanext=DateTime.now();
+    //
     // int peroidtime= int.parse(peroid.value_config);
     // datanext=Jiffy().add(days: peroidtime).dateTime;
     // print(datanext.toString());
-    // datanext.add(Duration(days: peroidtime));
+    // // datanext.add(Duration(days: peroidtime));
     // print(datanext);
-
-    await  Provider.of<communication_vm>
-    (context,listen: false).updatecarecommuncation({
-    'type':'دوري',
-    'fk_user':Provider.of<user_vm_provider>
-    (context,listen: false).
-    currentUser.idUser.toString(),
-    'date_communication':DateTime.now().toString(),
-    'result':typepayController.toString(),//
-    'rate':rate.toString(),
-    'number_wrong':numberwrong.toString(),
-    'client_repeat':repeat.toString(),
-    'date_next':datanext.toString(),
-    },widget.element.idCommunication);
-      // clear(val);
-    }  }
-
-                    },
-                  child: Text(' تم التواصل ',
-
-                    style: TextStyle(color:widget.element.dateCommunication==null?kWhiteColor: kMainColor),)) ,
-            ],
-          ),
-        ),
-      )
-  :  Container();
+    // print( widget.element.dateCommunication.toString());
+    // print( widget.element.idCommunication);
+    // if(widget.element.dateCommunication==null) {
+    // //datanext.add( Duration(days: day+ pp));
+    // // int peroidtime= int.parse(peroid.value_config);
+    // // datanext=Jiffy().add(days: peroidtime).dateTime;
+    // // print(datanext.toString());
+    // // datanext.add(Duration(days: peroidtime));
+    // // print(datanext);
+    //
+    // await  Provider.of<communication_vm>
+    // (context,listen: false).updatecarecommuncation({
+    // 'type':'دوري',
+    // 'fk_user':Provider.of<user_vm_provider>
+    // (context,listen: false).
+    // currentUser.idUser.toString(),
+    // 'date_communication':DateTime.now().toString(),
+    // 'result':typepayController.toString(),//
+    // 'rate':rate.toString(),
+    // 'number_wrong':numberwrong.toString(),
+    // 'client_repeat':repeat.toString(),
+    // 'date_next':datanext.toString(),
+    // },widget.element.idCommunication);
+    //   // clear(val);
+    // }  }
+    //
+    //                 },
+    //               child: Text(' تم التواصل ',
+    //
+    //                 style: TextStyle(color:widget.element.dateCommunication==null?kWhiteColor: kMainColor),)) ,
+    //         ],
+    //       ),
+    //     ),
+//      )
+         :  Container();
 
 
   }
