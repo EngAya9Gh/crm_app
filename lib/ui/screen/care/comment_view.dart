2bfaea5ea843104d417085a29151fb546b9dd39a
Mutafa.Hasria <mustafa.hasria.dev@gmail.com>
2023-08-09 16:06:23 +0300

    * Fix comment from client installation table.

--- lib/ui/screen/care/comment_view.dart
+++ lib/ui/screen/care/comment_view.dart
@@ -34,13 +34,13 @@ class _commentViewState extends State<commentView> {
   final _globalKey = GlobalKey<FormState>();
 
   TextEditingController _comment = TextEditingController();
-  late String fk_client;
-  String? nameEnterprise;
+  // late String fk_client;
+  // String? nameEnterprise;
 
   @override
   void initState() {
-    fk_client = widget.client!.idClients.toString();
-    nameEnterprise = widget.client!.nameEnterprise.toString();
+    // fk_client = widget.client!.idClients.toString();
+    // nameEnterprise = widget.client!.nameEnterprise.toString();
     // WidgetsBinding.instance.addPostFrameCallback((_)async {
     //   Provider.of<comment_vm>(context, listen: false)
     //       .getComment(widget.fk_client);
@@ -61,122 +61,123 @@ class _commentViewState extends State<commentView> {
   Widget build(BuildContext context) {
     return Scaffold(
         body: SafeArea(
-          child: Padding(
-            padding: EdgeInsets.only(left: 10, right: 10, top: 10, bottom: 10),
-            child: Directionality(
-              textDirection: TextDirection.rtl,
-              child: ListView(children: [
-                Row(
-                  //mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Flexible(
-                      child: Form(
-                        key: _globalKey,
-                        child: EditTextFormField(
-                          vaild: (value) {
-                            if (value!.toString().trim().isEmpty) {
-                              return label_empty;
-                            }
-                          },
-                          maxline: 3,
-                          paddcustom: EdgeInsets.only(top: 20, left: 3, right: 3, bottom: 3),
-                          controller: _comment,
-                          hintText: 'إضافة تعليق',
-                          // keyboardType: TextInputType.multiline,
-                        ),
-                      ),
-                    ),
-                    Consumer<comment_vm>(
-                      builder: (context, value, child) {
-                        if (value.isloadadd) {
-                          return AnimatedPadding(
-                            duration: kTabScrollDuration,
-                            padding: const EdgeInsets.symmetric(horizontal: 4.0),
-                            child: Center(
-                              child: SizedBox(width: 20, height: 20, child: CircularProgressIndicator()),
-                            ),
-                          );
+      child: Padding(
+        padding: EdgeInsets.only(left: 10, right: 10, top: 10, bottom: 10),
+        child: Directionality(
+          textDirection: TextDirection.rtl,
+          child: ListView(children: [
+            Row(
+              //mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Flexible(
+                  child: Form(
+                    key: _globalKey,
+                    child: EditTextFormField(
+                      vaild: (value) {
+                        if (value!.toString().trim().isEmpty) {
+                          return label_empty;
                         }
-                        return IconButton(
-                            onPressed: () async {
-                              if (_globalKey.currentState!.validate()) {
-                                _globalKey.currentState!.save();
-
-                                Provider.of<comment_vm>(context, listen: false).addComment_vm(
-                                  {
-                                    'content': _comment.text,
-                                    'fk_user': await Provider.of<user_vm_provider>(context, listen: false)
-                                        .currentUser
-                                        .idUser
-                                        .toString(),
-                                    'fk_client': fk_client,
-                                    'fkuser_client': widget.client!.fkUser.toString(), //صتحب العميل
-                                    'nameUser': widget.client!.nameUser.toString(),
-                                    'date_comment':
-                                        //Utils.toDateTime(
-                                        DateTime.now().toString(),
-                                    //),
-                                    'nameUser':
-                                        Provider.of<user_vm_provider>(context, listen: false).currentUser.nameUser,
-                                    'img_image': '',
-                                    'name_enterprise': nameEnterprise
-                                  },
-                                  Provider.of<user_vm_provider>(context, listen: false).currentUser.img_image,
-                                ).then((value) {
-                                  if (widget.event != null && isFirstComment) {
-                                    context.read<EventProvider>().changeEventToDone(
-                                          event: widget.event!,
-                                          onLoading: () {},
-                                          onSuccess: () {},
-                                          onFailure: () {},
-                                        );
-                                    isFirstComment = false;
-                                  }
-                                });
-                                _comment.text = '';
-                              }
-                            },
-                            icon: Icon(Icons.send, color: kMainColor));
                       },
+                      maxline: 3,
+                      paddcustom: EdgeInsets.only(top: 20, left: 3, right: 3, bottom: 3),
+                      controller: _comment,
+                      hintText: 'إضافة تعليق',
+                      // keyboardType: TextInputType.multiline,
                     ),
-                  ],
-                ),
-                Container(
-                  height: MediaQuery.of(context).size.height * 0.68,
-                  child: Padding(
-                    padding: const EdgeInsets.only(left: 8, right: 8, top: 10.0, bottom: 5),
-                    child: Consumer<comment_vm>(builder: (context, value, child) {
-                      return value.isLoading
-                          ? Center(child: CircularProgressIndicator.adaptive())
-                          : value.listComments.isNotEmpty && value.isLoading
-                              ? Center(child: CircularProgressIndicator.adaptive())
-                              : value.listComments.length == 0
-                                  ? Text('')
-                                  : Column(
-                                      children: [
-                                        Expanded(
-                                          child: ListView.builder(
-                                              scrollDirection: Axis.vertical,
-                                              itemCount: value.listComments.length,
-                                              itemBuilder: (context, index) {
-                                                return SingleChildScrollView(
-                                                    child: Padding(
-                                                        padding: const EdgeInsets.all(2),
-                                                        child:
-                                                            //Text(''),
-                                                            cardcomment(
-                                                          commentmodel: value.listComments[index],
-                                                        )));
-                                              }),
-                                        ),
-                                      ],
-                                    );
-                    }),
                   ),
                 ),
-              ]),
+                Consumer<comment_vm>(
+                  builder: (context, value, child) {
+                    if (value.isloadadd) {
+                      return AnimatedPadding(
+                        duration: kTabScrollDuration,
+                        padding: const EdgeInsets.symmetric(horizontal: 4.0),
+                        child: Center(
+                          child: SizedBox(width: 20, height: 20, child: CircularProgressIndicator()),
+                        ),
+                      );
+                    }
+                    return IconButton(
+                        onPressed: () async {
+                          if (_globalKey.currentState!.validate()) {
+                            _globalKey.currentState!.save();
+
+                            Provider.of<comment_vm>(context, listen: false).addComment_vm(
+                              {
+                                'content': _comment.text,
+                                'fk_user': await Provider.of<user_vm_provider>(context, listen: false)
+                                    .currentUser
+                                    .idUser
+                                    .toString(),
+                                'fk_client': widget.client!.idClients!,
+                                'fkuser_client': widget.client!.fkUser.toString(), //صتحب العميل
+                                'nameUser': widget.client!.nameUser.toString(),
+                                'date_comment':
+                                    //Utils.toDateTime(
+                                    DateTime.now().toString(),
+                                //),
+                                'nameUser': Provider.of<user_vm_provider>(context, listen: false).currentUser.nameUser,
+                                'img_image': '',
+                                'name_enterprise': widget.client!.nameEnterprise!
+                              },
+                              Provider.of<user_vm_provider>(context, listen: false).currentUser.img_image,
+                            ).then((value) {
+                              if (value != "error") {
+                                if (widget.event != null && isFirstComment) {
+                                  context.read<EventProvider>().changeEventToDone(
+                                        event: widget.event!,
+                                        onLoading: () {},
+                                        onSuccess: () {},
+                                        onFailure: () {},
+                                      );
+                                  isFirstComment = false;
+                                }
+                                _comment.text = '';
+                              }
+                            });
+                          }
+                        },
+                        icon: Icon(Icons.send, color: kMainColor));
+                  },
+                ),
+              ],
             ),
-          ),
-        ));
+            Container(
+              height: MediaQuery.of(context).size.height * 0.68,
+              child: Padding(
+                padding: const EdgeInsets.only(left: 8, right: 8, top: 10.0, bottom: 5),
+                child: Consumer<comment_vm>(builder: (context, value, child) {
+                  return value.isLoading
+                      ? Center(child: CircularProgressIndicator.adaptive())
+                      : value.listComments.isNotEmpty && value.isLoading
+                          ? Center(child: CircularProgressIndicator.adaptive())
+                          : value.listComments.length == 0
+                              ? Text('')
+                              : Column(
+                                  children: [
+                                    Expanded(
+                                      child: ListView.builder(
+                                          scrollDirection: Axis.vertical,
+                                          itemCount: value.listComments.length,
+                                          itemBuilder: (context, index) {
+                                            return SingleChildScrollView(
+                                                child: Padding(
+                                                    padding: const EdgeInsets.all(2),
+                                                    child:
+                                                        //Text(''),
+                                                        cardcomment(
+                                                      commentmodel: value.listComments[index],
+                                                    )));
+                                          }),
+                                    ),
+                                  ],
+                                );
+                }),
+              ),
+            ),
+          ]),
+        ),
+      ),
+    ));
   }
 }
