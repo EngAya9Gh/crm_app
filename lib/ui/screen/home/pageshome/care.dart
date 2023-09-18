7536f3378c017b1e1a081f1c83a476e76da71da2
CMD <eng.aya93g@gmail.com>
2023-09-18 15:07:17 +0300

    add prev care star

--- lib/ui/screen/home/pageshome/care.dart
+++ lib/ui/screen/home/pageshome/care.dart
@@ -115,6 +115,8 @@ class _carepageState extends State<carepage> {
                         care_page_view()));
           }, title: 'التواصل الدوري'):Container(),
 
+              Provider.of<privilge_vm>(context,listen: true)
+                  .checkprivlge('137')==true?
               buildSelectCategory(
                 colorbag: Colors.white,
                 colortitle: Colors.black,
@@ -123,7 +125,7 @@ class _carepageState extends State<carepage> {
                   Navigator.push(context, CupertinoPageRoute(builder: (context) => CommunicationListPage()));
                 },
                 title: 'قائمة العملاء المميزين',
-              ) ,
+              ):Container() ,
           Provider.of<privilge_vm>(context,listen: true)
               .checkprivlge('29')==true?
           buildSelectCategory(
