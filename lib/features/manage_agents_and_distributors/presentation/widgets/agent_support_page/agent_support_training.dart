2bc01d48ac5d744ecf90ebb8c303a28625f8c708
CMD <eng.aya93g@gmail.com>
2024-02-27 16:39:33 +0200

    replace api live

--- lib/features/manage_agents_and_distributors/presentation/widgets/agent_support_page/agent_support_training.dart
+++ lib/features/manage_agents_and_distributors/presentation/widgets/agent_support_page/agent_support_training.dart
@@ -46,7 +46,7 @@ class _AgentSupportTrainingState extends State<AgentSupportTraining> {
         final AgentDistributorModel trainer = bloc.traineeAgent!;
         return Column(
           children: [
-            if (trainer.nameusertraining != null)
+            if (trainer.nameusertraining != true)
               cardRow(
                   title: "موظف التدريب",
                   value: trainer.nameusertraining.toString()),
