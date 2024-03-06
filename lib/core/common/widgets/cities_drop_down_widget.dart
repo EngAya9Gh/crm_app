114e084212e1fa1a3b5df3a1759a55569471b185
Eng. Abdullah Elbokl <abdullah.bokl@gmail.com>
2024-03-06 17:59:58 +0200

    edit/ edit ui

--- lib/core/common/widgets/cities_drop_down_widget.dart
+++ lib/core/common/widgets/cities_drop_down_widget.dart
@@ -4,7 +4,6 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 
 import '../../../features/app/presentation/widgets/app_text.dart';
 import '../../../model/maincitymodel.dart';
-import '../../utils/app_styles.dart';
 import '../manager/cities_cubit/cities_cubit.dart';
 
 class CitiesDropDownWidget extends StatelessWidget {
@@ -15,76 +14,67 @@ class CitiesDropDownWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final cubit = context.read<CitiesCubit>();
-    return Container(
-      child: ClipRRect(
-        borderRadius: BorderRadius.circular(10),
-        child: DropdownButtonFormField<CityModel?>(
-          isExpanded: true,
-          validator: (value) {
-            if (value == null) {
-              return "هذا الحقل مطلوب";
-            }
-            return null;
-          },
-          icon: Icon(
-            Icons.keyboard_arrow_down_rounded,
-            color: Colors.grey,
-          ),
-          decoration: InputDecoration(
-            filled: true,
-            fillColor: Colors.grey.shade300,
-            border: InputBorder.none,
-            enabledBorder: InputBorder.none,
-            disabledBorder: InputBorder.none,
-            errorBorder: InputBorder.none,
-            focusedBorder: InputBorder.none,
-            focusedErrorBorder: InputBorder.none,
-          ),
-          hint: Text("حدد المدينة"),
-          items: cubit.citiesList
-              .toSet()
-              .map<DropdownMenuItem<CityModel?>>((CityModel? city) {
-            return DropdownMenuItem<CityModel?>(
-              alignment: Alignment.centerRight,
-              child: Container(
-                margin: const EdgeInsetsDirectional.all(2),
-                decoration: AppStyles.customBoxDecoration,
-                child: ListTile(
-                  trailing: Text(
-                    city?.name_city ?? '',
-                    textDirection: TextDirection.rtl,
-                  ),
-                ),
-              ),
-              value: city,
-            );
-          }).toList(),
-          value: cubit.selectedCity,
-          selectedItemBuilder: (context) {
-            return cubit.citiesList.toSet().map((item) {
-              return DropdownMenuItem<CityModel>(
-                value: item,
-                alignment: Alignment.centerRight,
-                child: AppText(
-                  item.name_city,
-                  style: context.textTheme.titleSmall,
-                ),
-              );
-            }).toList();
-          },
-          onChanged: (city) {
-            if (city == null) {
-              return;
-            }
-            cubit.selectedCity = city;
-          },
-          onSaved: (city) {
-            if (city == null) {
-              return;
-            }
-            cubit.selectedCity = city;
-          },
+    return ClipRRect(
+      borderRadius: BorderRadius.circular(10),
+      child: DropdownButtonFormField<CityModel?>(
+        itemHeight: 50,
+        decoration: InputDecoration(
+          filled: true,
+          fillColor: Colors.grey.shade200,
+          border: InputBorder.none,
+          enabledBorder: InputBorder.none,
+          disabledBorder: InputBorder.none,
         ),
+        isExpanded: true,
+        validator: (value) {
+          if (value == null) {
+            return "هذا الحقل مطلوب";
+          }
+          return null;
+        },
+        icon: Icon(
+          Icons.keyboard_arrow_down_rounded,
+          color: Colors.grey,
+        ),
+        hint: Text("حدد المدينة"),
+        items: cubit.citiesList
+            .map<DropdownMenuItem<CityModel?>>((CityModel? city) {
+          return DropdownMenuItem<CityModel?>(
+            alignment: Alignment.centerRight,
+            child: ListTile(
+              trailing: Text(
+                city?.name_city ?? '',
+                textDirection: TextDirection.rtl,
+              ),
+            ),
+            value: city,
+          );
+        }).toList(),
+        value: cubit.selectedCity,
+        selectedItemBuilder: (context) {
+          return cubit.citiesList.map((item) {
+            return DropdownMenuItem<CityModel>(
+              value: item,
+              alignment: Alignment.centerRight,
+              child: AppText(
+                item.name_city,
+                style: context.textTheme.titleSmall,
+              ),
+            );
+          }).toList();
+        },
+        onChanged: (city) {
+          if (city == null) {
+            return;
+          }
+          cubit.selectedCity = city;
+        },
+        onSaved: (city) {
+          if (city == null) {
+            return;
+          }
+          cubit.selectedCity = city;
+        },
       ),
     );
   }
