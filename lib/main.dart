9f2a5161da1a6c8d4d759b37e09e24853ff90eb5
Ahmad AL-Haidar <homamhaidar95@gmail.com>
2025-02-06 16:44:52 +0300

    build aab and fix error ios name project firebase default

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ import 'services/service_provider.dart';
 
 @pragma('vm:entry-point')
 Future<void> _firebaseOnBackgroundListener(RemoteMessage message) async {
-  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform,name: 'smart_crm');
 }
 
 void main() async {
